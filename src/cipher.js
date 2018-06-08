b8575304b70d6667e7dd7d9d8ebaf7103716dc8e
majohacelatarea <soberanesmajo@gmail.com>
2018-06-08 15:14:19 -0500

    Primera versión Cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,25 @@
-window.cipher = {
-  // ...
-};
+//window.cipher = {
+//}
+
+const originMessageSave = () => {
+  let originMessage = document.getElementById("message").value.toUpperCase();
+  let toDisplace = parseInt(document.getElementById("number").value);
+  let cipherText ="";
+  for(i = 0; i < originMessage.length; i++){
+    //console.log(originMessage[i]); 
+    let position = originMessage.charCodeAt(i);
+    //console.log(position) //Prueba y sí funciona
+    let formula = ((position - 65 + toDisplace) % 26 + 65);
+    //console.log(formula);// No puedo comprobar la fórmula
+    let newPosition = String.fromCharCode(formula);
+    //console.log(newPosition);
+    cipherText = cipherText + newPosition; 
+
+  }
+
+    //console.log(cipherText);
+  let result = document.getElementById("result").defaultValue = cipherText;
+}
+
+
+
---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+/*let position = originMessage.charCodeAt(i);
+let formula = (i - 65 + toDisplace) % 26 + 65;
+let newPosition = String.fromCharCode(formula);
+console.log(i);*/
+  
